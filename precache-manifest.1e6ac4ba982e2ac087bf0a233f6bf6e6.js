ef2dbb515544cc4aeefb53f4ea0dea57efb2aa19
harishdeivanayagam <harish.deiva@gmail.com>
2020-04-04 19:05:29 +0530

    Updates

---
+++ precache-manifest.1e6ac4ba982e2ac087bf0a233f6bf6e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e51736be6b9c9d8b97f5d17678232ca",
+    "url": "/HarishDeivanayagam/habit-tracker.git/index.html"
+  },
+  {
+    "revision": "8ea2863460f0ec8387b3",
+    "url": "/HarishDeivanayagam/habit-tracker.git/static/css/main.c55743f1.chunk.css"
+  },
+  {
+    "revision": "8ad4f7229665ff95c0ce",
+    "url": "/HarishDeivanayagam/habit-tracker.git/static/js/2.09991bea.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/HarishDeivanayagam/habit-tracker.git/static/js/2.09991bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea2863460f0ec8387b3",
+    "url": "/HarishDeivanayagam/habit-tracker.git/static/js/main.8b298381.chunk.js"
+  },
+  {
+    "revision": "63e9bc5da541e00e81e7",
+    "url": "/HarishDeivanayagam/habit-tracker.git/static/js/runtime-main.0478f7ab.js"
+  }
+]);
