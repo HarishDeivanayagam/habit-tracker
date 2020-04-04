f4e33c0c8e0c0d10449ca5907402294bb2b89a06
harishdeivanayagam <harish.deiva@gmail.com>
2020-04-04 19:19:29 +0530

    Updates

---
+++ precache-manifest.3b6bc94ada466d238ae75ff8db7928b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4979ee6477b756d40db9a058edee75c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae31c1502b52af93a8a",
+    "url": "/static/css/main.c55743f1.chunk.css"
+  },
+  {
+    "revision": "e08e79870712778c648e",
+    "url": "/static/js/2.dc142186.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.dc142186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae31c1502b52af93a8a",
+    "url": "/static/js/main.a71de323.chunk.js"
+  },
+  {
+    "revision": "46f2f084fb684dfc62c9",
+    "url": "/static/js/runtime-main.861adab9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HarishDeivanayagam/habit-tracker.git/precache-manifest.1e6ac4ba982e2ac087bf0a233f6bf6e6.js"
+  "/precache-manifest.3b6bc94ada466d238ae75ff8db7928b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HarishDeivanayagam/habit-tracker.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
